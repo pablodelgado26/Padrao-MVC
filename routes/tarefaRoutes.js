c7a10aef5c9fc0c5fdc117b323ecf82f967355fb
{Pablo Delgado} <{pablo.j.abreu@aluno.senai.br}>
2025-02-27 11:27:43 -0300

    feat: termino de rotas

--- routes/tarefaRoutes.js
+++ routes/tarefaRoutes.js
@@ -4,3 +4,8 @@ import tarefaController from '../controllers/tarefaController.js';
 const router = express.Router();
 
 router.get('/', tarefaController.getAll);
+router.post('/', tarefaController.create);
+router.put('/:id', tarefaController.update);
+router.delete('/:id', tarefaController.delete);
+
+export default router;
